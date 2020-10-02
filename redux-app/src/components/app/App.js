b585d5c9a4c9e8d80674042e3be76fafc1d2a5a9
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-10-02 23:58:02 +0300

    add routing

--- redux-app/src/components/app/App.js
+++ redux-app/src/components/app/App.js
@@ -2,15 +2,24 @@ import React from 'react';
 import AppHeader from '../appHeader';
 import { MainPage, CartPage } from '../pages';
 import './app.scss';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+} from "react-router-dom";
 
 function App() {
 	return (
 		<>
-			<AppHeader />
-			<div className='Wrapper'>
-				<MainPage />
-				<CartPage />
-			</div>
+			<Router>
+				<AppHeader />
+				<Switch>
+					<div className='Wrapper'>
+						<Route exact path="/"><MainPage /></Route>
+						<Route path="/cart"><CartPage /></Route>
+					</div>
+				</Switch>
+			</Router>
 		</>
 	);
 }
--- redux-app/src/components/appHeader/index.js
+++ redux-app/src/components/appHeader/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './appHeader.scss';
+import { Link } from "react-router-dom";
 
 const AppHeader = () => {
 	return (
 		<div className='navWrapper'>
 			<div className='navBar'>
 				<ul className='navList'>
-					<li className='navItem'>Main Page</li>
-					<li className='navItem'>Cart</li>
+					<li className='navItem'><Link to='/'>Main Page</Link></li>
+					<li className='navItem'><Link to='/cart'>Cart</Link></li>
 					<li className='navItem'>Total: 50$</li>
 				</ul>
 			</div>
