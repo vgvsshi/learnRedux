d9f979d1fa5edbc0f58ac6e03068212856d4f879
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-10-03 16:24:59 +0300

    add total price

--- redux-app/src/components/appHeader/index.js
+++ redux-app/src/components/appHeader/index.js
@@ -4,13 +4,17 @@ import { Link } from "react-router-dom";
 import { connect } from 'react-redux';
 
 const AppHeader = ({ items }) => {
+	let total = 0;
+	items.map((item) => {
+		return total = total + item.price * item.amount;
+	})
 	return (
 		<div className='navWrapper'>
 			<div className='navBar'>
 				<ul className='navList'>
 					<li className='navItem'><Link to='/'>Main Page</Link></li>
 					<li className='navItem'><Link to='/cart'>Cart</Link></li>
-					<li className='navItem'>Total: {items.map()}$</li>
+					<li className='navItem'>Total: {total}$</li>
 				</ul>
 			</div>
 		</div>
